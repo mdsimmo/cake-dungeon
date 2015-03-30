71b13d256c311cc913c3c4e58a582037d243511e
mdsimmo <mdsimmo2+git@gmail.com>
2015-03-30 18:13:51 +1100

    added more images

--- java/com/github/mdsimmo/pixeldungeon/sprites/ItemSpriteSheet.java
+++ java/com/github/mdsimmo/pixeldungeon/sprites/ItemSpriteSheet.java
@@ -18,163 +18,177 @@
 package com.github.mdsimmo.pixeldungeon.sprites;
 
 public class ItemSpriteSheet {
-	
-	// Placeholders
-	public static final int WEAPON	= 5;
-	public static final int ARMOR	= 6;
-	public static final int RING	= 7;
-	public static final int SMTH	= 127;
-	
-	// Keys
-	public static final int SKELETON_KEY	= 8;
-	public static final int IRON_KEY		= 9;
-	public static final int GOLDEN_KEY		= 10;
-	
-	// Melee weapons
-	public static final int SHORT_SWORD		= 2;
-	public static final int KNUCKLEDUSTER	= 16;
-	public static final int QUARTERSTAFF	= 17;
-	public static final int MACE			= 18;
-	public static final int DAGGER			= 19;
-	public static final int SWORD			= 20;
-	public static final int LONG_SWORD		= 21;
-	public static final int BATTLE_AXE		= 22;
-	public static final int WAR_HAMMER		= 23;
-	public static final int SPEAR			= 29;
-	public static final int GLAIVE			= 30;
-	
-	// Missiles
-	public static final int SHURIKEN		= 15;
-	public static final int DART			= 31;
-	public static final int BOOMERANG		= 106;
-	public static final int TOMAHAWK		= 107;
-	public static final int INCENDIARY_DART	= 108;
-	public static final int CURARE_DART		= 109;
-	public static final int JAVELIN			= 110;
-	
-	// Armors
-	public static final int ARMOR_CLOTH		= 24;
-	public static final int ARMOR_LEATHER	= 25;
-	public static final int ARMOR_MAIL		= 26;
-	public static final int ARMOR_SCALE		= 27;
-	public static final int ARMOR_PLATE		= 28;
-	public static final int ARMOR_ROGUE		= 96;
-	public static final int ARMOR_WARRIOR	= 97;
-	public static final int ARMOR_MAGE		= 98;
-	public static final int ARMOR_HUNTRESS	= 99;
-	
-	// Wands
-	public static final int WAND_MAGIC_MISSILE	= 3;
-	public static final int WAND_HOLLY			= 48;
-	public static final int WAND_YEW			= 49;
-	public static final int WAND_EBONY			= 50;
-	public static final int WAND_CHERRY			= 51;
-	public static final int WAND_TEAK			= 52;
-	public static final int WAND_ROWAN			= 53;
-	public static final int WAND_WILLOW			= 54;
-	public static final int WAND_MAHOGANY		= 55;
-	public static final int WAND_BAMBOO			= 68;
-	public static final int WAND_PURPLEHEART	= 69;
-	public static final int WAND_OAK			= 70;
-	public static final int WAND_BIRCH			= 71;
-	
-	// Rings
-	public static final int RING_DIAMOND	= 32;
-	public static final int RING_OPAL		= 33;
-	public static final int RING_GARNET		= 34;
-	public static final int RING_RUBY		= 35;
-	public static final int RING_AMETHYST	= 36;
-	public static final int RING_TOPAZ		= 37;
-	public static final int RING_ONYX		= 38;
-	public static final int RING_TOURMALINE	= 39;
-	public static final int RING_EMERALD	= 72;
-	public static final int RING_SAPPHIRE	= 73;
-	public static final int RING_QUARTZ		= 74;
-	public static final int RING_AGATE		= 75;
-	
-	// Potions
-	public static final int POTION_TURQUOISE	= 56;
-	public static final int POTION_CRIMSON		= 57;
-	public static final int POTION_AZURE		= 58;
-	public static final int POTION_JADE			= 59;
-	public static final int POTION_GOLDEN		= 60;
-	public static final int POTION_MAGENTA		= 61;
-	public static final int POTION_CHARCOAL		= 62;
-	public static final int POTION_IVORY		= 63;
-	public static final int POTION_AMBER		= 64;
-	public static final int POTION_BISTRE		= 65;
-	public static final int POTION_INDIGO		= 66;
-	public static final int POTION_SILVER		= 67;
-	
-	// Scrolls
-	public static final int SCROLL_KAUNAN	= 40;
-	public static final int SCROLL_SOWILO	= 41;
-	public static final int SCROLL_LAGUZ	= 42;
-	public static final int SCROLL_YNGVI	= 43;
-	public static final int SCROLL_GYFU		= 44;
-	public static final int SCROLL_RAIDO	= 45;
-	public static final int SCROLL_ISAZ		= 46;
-	public static final int SCROLL_MANNAZ	= 47;
-	public static final int SCROLL_NAUDIZ	= 76;
-	public static final int SCROLL_BERKANAN	= 77;
-	public static final int SCROLL_ODAL		= 78;
-	public static final int SCROLL_TIWAZ	= 79;
-	
-	// Seeds
-	public static final int SEED_FIREBLOOM	= 88;
-	public static final int SEED_ICECAP		= 89;
-	public static final int SEED_SORROWMOSS	= 90;
-	public static final int SEED_DREAMWEED	= 91;
-	public static final int SEED_SUNGRASS	= 92;
-	public static final int SEED_EARTHROOT	= 93;
-	public static final int SEED_FADELEAF	= 94;
-	public static final int SEED_ROTBERRY	= 95;
-	
-	// Quest items
-	public static final int ROSE	= 100;
-	public static final int PICKAXE	= 101;
-	public static final int ORE		= 102;
-	public static final int SKULL	= 103;
-	public static final int DUST	= 121;
-	public static final int TOKEN	= 122;
-	
-	// Heaps
-	public static final int BONES			= 0;
-	public static final int CHEST			= 11;
-	public static final int LOCKED_CHEST	= 12;
-	public static final int TOMB			= 13;
-	public static final int CRYSTAL_CHEST	= 105;
-	
-	// Food
-	public static final int RATION		= 4;
-	public static final int PASTY		= 112;
-	public static final int MEAT		= 113;
-	public static final int STEAK		= 114;
-	public static final int OVERPRICED	= 115;
-	public static final int CARPACCIO	= 116;
-	
-	// Bags
-	public static final int POUCH	= 83;
-	public static final int HOLDER	= 104;
-	public static final int HOLSTER	= 111;
-	public static final int KEYRING	= 126;
-	
-	// UNUSED
-	public static final int SPARK_UPGRADE	= 117;
-	public static final int SPARK_ENCHANT	= 118;
-	
-	// Misc
-	public static final int ANKH	= 1;
-	public static final int GOLD	= 14;
-	public static final int STYLUS	= 80;
-	public static final int DEWDROP	= 81;
-	public static final int MASTERY	= 82;
-	public static final int TORCH	= 84;
-	public static final int BEACON	= 85;
-	public static final int KIT		= 86;
-	public static final int AMULET	= 87;
-	public static final int VIAL	= 120;
-	public static final int WEIGHT	= 123;
-	public static final int BOMB	= 124;
-	public static final int HONEYPOT= 125;
+    
+    // Placeholders
+    public static final int WEAPON	= 5;
+    public static final int ARMOR	= 6;
+    public static final int RING	= 7;
+    public static final int SMTH	= 8;
+
+    public static final int GOLD	= 2;
+    
+    // Keys
+    public static final int SKELETON_KEY	= 16;
+    public static final int IRON_KEY		= 17;
+    public static final int GOLDEN_KEY		= 18;
+    public static final int KEYRING	        = 19;
+
+    // Heaps
+    public static final int CHEST			= 21;
+    public static final int LOCKED_CHEST	= 22;
+    public static final int TOMB			= 23;
+    public static final int CRYSTAL_CHEST	= 24;
+    public static final int BONES			= 25;
+    
+    // Melee weapons
+    public static final int SHORT_SWORD		= 32;
+    public static final int KNUCKLEDUSTER	= 33;
+    public static final int QUARTERSTAFF	= 34;
+    public static final int MACE			= 35;
+    public static final int DAGGER			= 36;
+    public static final int SWORD			= 37;
+    public static final int LONG_SWORD		= 38;
+    public static final int BATTLE_AXE		= 39;
+    public static final int WAR_HAMMER		= 40;
+    public static final int SPEAR			= 41;
+    public static final int GLAIVE			= 42;
+    
+    // Missiles
+    public static final int DART			= 208;
+    public static final int SHURIKEN		= 209;
+    public static final int BOOMERANG		= 210;
+    public static final int TOMAHAWK		= 211;
+    public static final int INCENDIARY_DART	= 212;
+    public static final int CURARE_DART		= 213;
+    public static final int JAVELIN			= 214;
+    public static final int BOMB	        = 215;
+    
+    // Armors
+    public static final int ARMOR_CLOTH		= 48;
+    public static final int ARMOR_LEATHER	= 49;
+    public static final int ARMOR_MAIL		= 50;
+    public static final int ARMOR_SCALE		= 51;
+    public static final int ARMOR_PLATE		= 52;
+    public static final int ARMOR_ROGUE		= 53;
+    public static final int ARMOR_WARRIOR	= 54;
+    public static final int ARMOR_MAGE		= 55;
+    public static final int ARMOR_HUNTRESS	= 56;
+    
+    // Wands
+    public static final int WAND_HOLLY			= 96;
+    public static final int WAND_YEW			= 97;
+    public static final int WAND_EBONY			= 98;
+    public static final int WAND_CHERRY			= 99;
+    public static final int WAND_TEAK			= 100;
+    public static final int WAND_ROWAN			= 101;
+    public static final int WAND_WILLOW			= 102;
+    public static final int WAND_MAHOGANY		= 103;
+    public static final int WAND_BAMBOO			= 104;
+    public static final int WAND_PURPLEHEART	= 105;
+    public static final int WAND_OAK			= 106;
+    public static final int WAND_BIRCH			= 107;
+    public static final int WAND_MAGIC_MISSILE	= 108;
+    
+    // Rings
+    public static final int RING_DIAMOND	= 64;
+    public static final int RING_OPAL		= 65;
+    public static final int RING_GARNET		= 66;
+    public static final int RING_RUBY		= 67;
+    public static final int RING_AMETHYST	= 68;
+    public static final int RING_TOPAZ		= 69;
+    public static final int RING_ONYX		= 70;
+    public static final int RING_TOURMALINE	= 71;
+    public static final int RING_EMERALD	= 72;
+    public static final int RING_SAPPHIRE	= 73;
+    public static final int RING_QUARTZ		= 74;
+    public static final int RING_AGATE		= 75;
+    
+    // Potions
+    public static final int POTION_TURQUOISE	= 112;
+    public static final int POTION_CRIMSON		= 113;
+    public static final int POTION_AZURE		= 114;
+    public static final int POTION_JADE			= 115;
+    public static final int POTION_GOLDEN		= 116;
+    public static final int POTION_MAGENTA		= 117;
+    public static final int POTION_CHARCOAL		= 118;
+    public static final int POTION_IVORY		= 119;
+    public static final int POTION_AMBER		= 120;
+    public static final int POTION_BISTRE		= 121;
+    public static final int POTION_INDIGO		= 122;
+    public static final int POTION_SILVER		= 123;
+    
+    // Scrolls
+    public static final int SCROLL_KAUNAN	= 80;
+    public static final int SCROLL_SOWILO	= 81;
+    public static final int SCROLL_LAGUZ	= 82;
+    public static final int SCROLL_YNGVI	= 83;
+    public static final int SCROLL_GYFU		= 84;
+    public static final int SCROLL_RAIDO	= 85;
+    public static final int SCROLL_ISAZ		= 86;
+    public static final int SCROLL_MANNAZ	= 87;
+    public static final int SCROLL_NAUDIZ	= 86;
+    public static final int SCROLL_BERKANAN	= 87;
+    public static final int SCROLL_ODAL		= 88;
+    public static final int SCROLL_TIWAZ	= 89;
+    
+    // Seeds
+    public static final int SEED_FIREBLOOM	= 176;
+    public static final int SEED_ICECAP		= 177;
+    public static final int SEED_SORROWMOSS	= 178;
+    public static final int SEED_DREAMWEED	= 179;
+    public static final int SEED_SUNGRASS	= 180;
+    public static final int SEED_EARTHROOT	= 181;
+    public static final int SEED_FADELEAF	= 182;
+    public static final int SEED_ROTBERRY	= 183;
+    
+    // Quest items
+    public static final int ROSE	= 192;
+    public static final int PICKAXE	= 193;
+    public static final int ORE		= 194;
+    public static final int SKULL	= 195;
+    public static final int DUST	= 196;
+    public static final int TOKEN	= 197;
+    
+    // Food
+    public static final int RATION		= 128;
+    public static final int PASTY		= 129;
+    public static final int MEAT		= 130;
+    public static final int STEAK		= 131;
+    public static final int OVERPRICED	= 132;
+    public static final int CARPACCIO	= 133;
+
+    public static final int CAKE        = 144;
+    public static final int BEAN        = 145;
+    public static final int EGG         = 146;
+    public static final int SUGAR       = 147;
+    public static final int COOKED_CHOP = 148;
+    public static final int LAMB_CHOP   = 149;
+    public static final int BACON       = 150;
+    public static final int COOKED_BACON= 151;
+    public static final int FRIED_EGG   = 152;
+    public static final int SCRAMBLED   = 153;
+    public static final int DONUT       = 154;
+
+    // Bags
+    public static final int POUCH	= 224;
+    public static final int HOLDER	= 225;
+    public static final int HOLSTER	= 226;
+
+    // UNUSED
+    public static final int SPARK_UPGRADE	= 229;
+    public static final int SPARK_ENCHANT	= 230;
+    
+    // Misc
+    public static final int ANKH	= 160;
+    public static final int DEWDROP	= 161;
+    public static final int VIAL	= 162;
+    public static final int TORCH	= 163;
+    public static final int BEACON	= 164;
+    public static final int KIT		= 165;
+    public static final int AMULET	= 167;
+    public static final int MASTERY	= 168;
+    public static final int WEIGHT	= 169;
+    public static final int HONEYPOT= 170;
+    public static final int RECIPIE = 171;
+    public static final int STYLUS  = 172;
 }
